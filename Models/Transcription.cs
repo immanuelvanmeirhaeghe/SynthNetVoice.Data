3819382300e7ac346ea5e622c897c56a1374d18e
immanuelvanmeirhaeghe <immanuel.van.meirhaeghe@gmail.com>
2023-09-30 02:44:50 +0200

    latest fixes

--- Models/Transcription.cs
+++ Models/Transcription.cs
@@ -3,12 +3,22 @@ using System.Text.Json.Serialization;
 using System.Windows;
 namespace SynthNetVoice.Data.Models
 {
+    /// <summary>
+    /// Transcription info
+    /// </summary>
     public class Transcription
     {
-        public string? Text { get; set; }
-
+        /// <summary>
+        /// File path with file name of input document containing text that will be transcribed to a sound file and spoken out.
+        /// </summary>
+        public string? TextFilePath { get; set; }
+        /// <summary>
+        /// File path where the transcribed audio file will be saved.
+        /// </summary>
         public string? AudioFilePath { get; set; }
-
+        /// <summary>
+        /// File name for the audio file with extension.
+        /// </summary>
         public string? AudioFileName { get; set; }
 
         [JsonIgnore]
